e3ade4c881cf56322251da7d080634d134761a52
Stefan Hyltoft <stefanhyltoft@gmail.com>
2016-05-03 16:34:09 +0200

    Implementeret sletning
    (tjekker ikke om databasen rent faktisk sletter genstand - sender kun
    DELETE.)

--- src/JerseyRESTClient.java
+++ src/JerseyRESTClient.java
@@ -1,10 +1,14 @@
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+
+import org.glassfish.jersey.client.ClientResponse;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -14,9 +18,10 @@ import com.google.gson.Gson;
 public class JerseyRESTClient {	
 	
 	public ArrayList<Item> itemList;
-	
-	public JerseyRESTClient() throws JSONException, IOException {
-		URL url = new URL("http://fbballin.com/v1/items/");
+	private String baseURL = "http://fbballin.com/v1/items/";
+		
+	public void getItemsFromAPI() throws JSONException, IOException {
+		URL url = new URL(baseURL);
 		JSONTokener tokener = new JSONTokener(url.openStream());
 		JSONObject root = new JSONObject(tokener);
 		Gson gson = new Gson();
@@ -49,11 +54,13 @@ public class JerseyRESTClient {
 		}
 	}	
 	
-	private void printMenu() {
+	private void printMenu() throws IOException, JSONException {
+		getItemsFromAPI();
 		System.out.println("Velkommen til Super Simple DDHF Java REST klient!");
 		System.out.println("Vælg en kommando:");
 		System.out.println("1. List alle genstande");
-		System.out.println("2. Slå genstand op via. ID");
+		System.out.println("2. Slå genstand op");
+		System.out.println("3. Slet genstand");
 		
 		Scanner scan = new Scanner(System.in);
 		String userInput = scan.nextLine();
@@ -67,10 +74,30 @@ public class JerseyRESTClient {
 			System.out.println(lookupItem(userInput));
 			userInput = scan.nextLine();
 			break;
+		case "3":
+			System.out.println("Indtast genstand id:");
+			userInput = scan.nextLine();
+			System.out.println("Indtast genstand id igen for at bekræfte sletning:");
+			String userInput2 = scan.nextLine();
+			if(userInput.equals(userInput2)) {
+				deleteItem(userInput);
+			} else {
+				System.out.println("Item id matchede ikke");
+			}
 		}
 		printMenu();
 	}
 	
+	private void deleteItem(String userInput) throws IOException {
+		URL url = new URL(baseURL + Integer.valueOf(userInput) + "?token=test");
+		HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+		httpCon.setDoOutput(true);
+		httpCon.setRequestProperty(
+		    "Content-Type", "application/x-www-form-urlencoded" );
+		httpCon.setRequestMethod("DELETE");
+		httpCon.connect();	
+	}
+
 	private String lookupItem(String userInput) {
 		for (int i = 0; i < itemList.size(); i++) {
 			if(itemList.get(i).getId() == Integer.valueOf(userInput)) {
@@ -95,7 +122,7 @@ public class JerseyRESTClient {
 		private String donator;
 		private String producer;
 		private String zipcode;
-		private JSONObject dating_to;
+//		private JSONObject dating_to;
 		
 		public String getDescription() { return description; }
 		
@@ -109,8 +136,8 @@ public class JerseyRESTClient {
 					"Beskrivelse: " + description + "\n" +
 					"Postnr.: " + zipcode + "\n" +
 					"Producent: " + producer + "\n" +
-					"Donator: " + donator + "\n" +
-					"Dateret fra: " + dating_to + "\n"
+					"Donator: " + donator + "\n" 
+//					"Dateret fra: " + dating_to + "\n"
 					;
 		}
 		
